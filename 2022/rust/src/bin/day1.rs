8a1c51042c29945243935036dd4ed3b04360f751
Chris <fleetscut@gmail.com>
2022-12-01 10:46:18 -0500

    2022 day1

--- 2022/rust/src/bin/day1.rs
+++ 2022/rust/src/bin/day1.rs
@@ -1,6 +1,41 @@
-use aoc::test_print;
+use std::fs::read_to_string;
+
+use aoc::*;
 
 fn main() {
-    println!("test");
-    test_print();
+    let day = 1;
+    let example_path = format!("./data/day{}ex.txt", day);
+    let input_path = format!("./data/day{}.txt", day);
+
+    let example_input = read_to_string(&example_path).expect("Could not open file");
+    let input = read_to_string(&input_path).expect("Could not open file");
+
+    let p1 = part_one(&example_input);
+    println!("Part One Example: {}", p1);
+    let p1 = part_one(&input);
+    println!("Part One: {}", p1);
+    println!("-----");
+    let p2 = part_two(&example_input);
+    println!("Part Two Example: {}", p2);
+    let p2 = part_two(&input);
+    println!("Part Two: {}", p2);
+}
+
+fn part_one(input: &str) -> i32 {
+    *get_calories(input).last().unwrap()
+}
+
+fn part_two(input: &str) -> i32 {
+    let mut calories = get_calories(input);
+    calories.reverse();
+    calories.iter().take(3).sum()
+}
+
+fn get_calories(input: &str) -> Vec<i32> {
+    let mut calories: Vec<i32> = input
+        .split("\n\n")
+        .map(|chunk| chunk.lines().map(|line| line.parse::<i32>().unwrap()).sum())
+        .collect();
+    calories.sort();
+    calories
 }
